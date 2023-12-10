97ddd60f5065a668b626f2688ae05bbfc69b73b4
JMUR <architect.lock@outlook.com>
2023-12-10 23:01:20 +0300

    working JWT generation and parsing

--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -2,44 +2,45 @@ package utils
 
 import (
 	"e-commerce/pkg/config"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
 )
 
-func GenerateToken(userID int64) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"exp": time.Now().Add(10 * time.Minute).Unix(),
-		"id":  userID,
-		"sub": "user",
-		"iss": "auth",
-		"jti": uuid.NewString(),
-		"nbf": time.Now().Unix(),
+type Claims struct {
+	jwt.RegisteredClaims
+	UserId int `json:"userId"`
+}
+
+func GenerateToken(userID int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+		UserId: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
 	})
 
-	tokenString, err := token.SignedString([]byte(config.JWTsecretKey))
+	ss, err := token.SignedString([]byte(config.JWTsecretKey))
 	if err != nil {
 		return "", fmt.Errorf("failed to sign token: %w", err)
 	}
 
-	return tokenString, nil
+	return ss, nil
 }
 
-func ParseToken(accessToken string) (*jwt.Token, error) {
-	token, err := jwt.ParseWithClaims(accessToken, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
-		_, ok := token.Method.(*jwt.SigningMethodHMAC)
-		if !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
+func ParseToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(config.JWTsecretKey), nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse token: %w", err)
+		return nil, fmt.Errorf("error: %v", err)
+	} else if claims, ok := token.Claims.(*Claims); ok {
+		return claims, nil
+	} else {
+		return nil, errors.New("unknown claims type, cannot proceed")
 	}
-	if !token.Valid {
-		return nil, fmt.Errorf("token is not valid")
-	}
-	return token, nil
 }
